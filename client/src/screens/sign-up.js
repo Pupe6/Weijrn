5be9267a1c5ce7521d0b054f69d9cfcdab2e00ae
Borislav Borisov <borislav1207@gmail.com>
2023-03-08 22:10:20 +0200

    resolving old conflict?

--- client/src/screens/sign-up.js
+++ client/src/screens/sign-up.js
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { Text, Box, Center, VStack } from "native-base";
+import { FormControl, Input, Button, Text } from "native-base";
 import ThemeToggle from "../components/common/theme-toggle";
+import { useForm } from "../hooks/useForm";
 
 export default function SignUpScreen() {
 	return (
