aadf92fea1c81f5d1f1674116090ab163dc81889
Borislav Borisov <borislav1207@gmail.com>
2023-03-08 22:11:35 +0200

    deleted items from folder renaming

--- weijrn-client/src/hooks/useForm.js
+++
@@ -1,6 +0,0 @@
-import React, { useState } from 'react';
-
-export default function useForm(initialState = {}) {
-    const [values, setValues] = useState(initialState);
-    const [errors, setErrors] = useState({});
-    const [touched, setTouched] = useState({});
