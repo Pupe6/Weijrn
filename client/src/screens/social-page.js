7c99ca03146f4078e65c76555383a62fbd1a7f7e
Kaloyan Anastasov <kaloanas07@gmail.com>
2023-03-09 10:11:00 +0200

    commit fuck up

---
+++ client/src/screens/social-page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { SafeAreaView, View, Text } from "react-native";
+
+export default function SocialPage(){
+    return(
+        <SafeAreaView >
+            <Text> Social page</Text>
+        </SafeAreaView>
+    )
+}
