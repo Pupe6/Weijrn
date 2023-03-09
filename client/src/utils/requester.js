28ccab58ee6558a715e2eaa0053dcdf52938de81
Borislav Borisov <borislav1207@gmail.com>
2023-03-09 13:28:24 +0200

    added requester util

---
+++ client/src/utils/requester.js
@@ -0,0 +1,28 @@
+const baseUrl = "";
+
+const requester = async (method, path, body = undefined) => {
+	const res = await fetch(`${baseUrl}${path}`, {
+		method,
+		mode: "cors",
+		headers: {
+			"Content-Type": "application/json",
+			"X-Token": JSON.parse(sessionStorage.getItem("user"))?._token || "",
+		},
+		body: method !== "GET" ? JSON.stringify(body) : body,
+	}).catch(err => {
+		throw err;
+	});
+
+	const response = await res.json();
+
+	if (!res.ok) {
+		throw response;
+	}
+
+	return response;
+};
+
+export const get = requester.bind(null, "GET");
+export const post = requester.bind(null, "POST");
+export const put = requester.bind(null, "PUT");
+export const del = requester.bind(null, "DELETE");
