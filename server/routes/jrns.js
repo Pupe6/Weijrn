dad91c569de68c40cb5f42369eeb395acf6a24f7
ProGamer2711 <alexandurshestakov@gmail.com>
2023-03-10 14:28:39 +0200

    added tag sharing

--- server/routes/jrns.js
+++ server/routes/jrns.js
@@ -1,11 +1,14 @@
 const Tag = require("../models/Tag");
 const User = require("../models/User");
+const ShareCode = require("../models/ShareCode");
 
 const verifyMAC = require("../middleware/verifyMAC");
 const verifyJWT = require("../middleware/verifyJWT");
 
-const router = require("express").Router();
 const { encryptTag } = require("../utils/encryption");
+const randomCodeGenerator = require("../utils/randomCodeGenerator");
+
+const router = require("express").Router();
 
 router.get("/tags", verifyMAC, async (req, res) => {
 	try {
@@ -128,6 +131,70 @@ router.delete("/tags/:nickname", verifyJWT, async (req, res) => {
 		);
 
 		res.status(200).json({ message: `Tag "${nickname}" deleted.` });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
+router.post("/tags/share/:nickname", verifyMAC, async (req, res) => {
+	try {
+		const { nickname } = req.params;
+		const user = req.user;
+
+		const tagId = user._tags.find(tag => tag.nickname === nickname)?._id;
+
+		if (!tagId)
+			return res
+				.status(404)
+				.json({ message: `Tag "${nickname}" not found.` });
+
+		let randomCode = randomCodeGenerator(6);
+
+		// regenerate code if it already exists
+		while (await ShareCode.findOne({ shareCode: randomCode }))
+			randomCode = randomCodeGenerator(6);
+
+		console.log(randomCode);
+
+		const shareCode = await ShareCode.create({
+			_tag: tagId,
+			shareCode: randomCode,
+		});
+
+		res.status(200).json({ shareCode });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
+router.get("/tags/share/:code", verifyMAC, async (req, res) => {
+	try {
+		const { code } = req.params;
+		const user = req.user;
+
+		const tag = (await ShareCode.findOne({ shareCode: code }))?._tag;
+
+		if (!tag)
+			return res
+				.status(404)
+				.json({ message: `Share code "${code}" not found.` });
+
+		// make sure user doesn't already have the tag
+		if (user._tags.some(userTag => userTag._id.equals(tag)))
+			return res
+				.status(400)
+				.json({ message: "You already have this tag." });
+
+		await User.findOneAndUpdate(
+			{ _id: user._id },
+			{ $push: { _tags: tag } },
+			{ new: true }
+		);
+
+		// remove share code
+		// await ShareCode.findOneAndDelete({ shareCode: code });
+
+		res.status(200).json({ message: "Tag shared." });
 	} catch (err) {
 		res.status(500).json({ message: err.message });
 	}
