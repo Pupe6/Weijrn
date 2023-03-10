3160e43de6a83d1f9178cb439701c8882192e46b
Borislav Borisov <borislav1207@gmail.com>
2023-03-10 20:09:23 +0200

    in the proccess of changing the app to use async storage instead of Secure Store

--- client/src/utils/requester.js
+++ client/src/utils/requester.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://5800-194-141-252-114.eu.ngrok.io";
+const baseUrl = "https://ffbc-95-42-52-106.eu.ngrok.io";
 // const baseUrl = "https://weijrnserver.onrender.com";
 
 const requester = async (
