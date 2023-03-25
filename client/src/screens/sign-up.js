5ef5592ff4e1cc8f45ad1471205d05081a3413f9
bobikenobi12 <borislav1207@gmail.com>
2023-03-25 19:06:17 +0200

    added theming for sign up

--- client/src/screens/sign-up.js
+++ client/src/screens/sign-up.js
@@ -38,126 +38,135 @@ export default function SignUpScreen({ navigation }) {
 	}, [isSubmitting]);
 
 	return (
-		<Center w="100%">
-			<Box safeArea p="2" w="90%" maxW="290" py="8">
-				<Heading
-					size="lg"
-					color="coolGray.800"
-					_dark={{
-						color: "warmGray.50",
-					}}
-					fontWeight="semibold">
-					Welcome
-				</Heading>
-				<Heading
-					mt="1"
-					color="coolGray.600"
-					_dark={{
-						color: "warmGray.200",
-					}}
-					fontWeight="medium"
-					size="xs">
-					Sign up to continue!
-				</Heading>
-				<VStack space={3} mt="5">
-					<FormControl isInvalid={errors?.username}>
-						<FormControl.Label>Username</FormControl.Label>
-						<Input
-							onChangeText={text =>
-								handleChange("username", text)
-							}
-						/>
+		<Box flex="1" safeArea bg={bg}>
+			<Center w="100%">
+				<Box safeArea p="2" w="90%" maxW="290" py="8">
+					<Heading
+						size="lg"
+						color="coolGray.800"
+						_dark={{
+							color: "warmGray.50",
+						}}
+						fontWeight="semibold">
+						Welcome
+					</Heading>
+					<Heading
+						mt="1"
+						color="coolGray.600"
+						_dark={{
+							color: "warmGray.200",
+						}}
+						fontWeight="medium"
+						size="xs">
+						Sign up to continue!
+					</Heading>
+					<VStack space={3} mt="5">
+						<FormControl isInvalid={errors?.username}>
+							<FormControl.Label>Username</FormControl.Label>
+							<Input
+								onChangeText={text =>
+									handleChange("username", text)
+								}
+							/>
 
-						<FormControl.ErrorMessage>
-							{errors?.username}
-						</FormControl.ErrorMessage>
-					</FormControl>
-					<FormControl isInvalid={errors?.email}>
-						<FormControl.Label>Email</FormControl.Label>
-						<Input
-							onChangeText={text => handleChange("email", text)}
-						/>
+							<FormControl.ErrorMessage>
+								{errors?.username}
+							</FormControl.ErrorMessage>
+						</FormControl>
+						<FormControl isInvalid={errors?.email}>
+							<FormControl.Label>Email</FormControl.Label>
+							<Input
+								onChangeText={text =>
+									handleChange("email", text)
+								}
+							/>
 
-						<FormControl.ErrorMessage>
-							{errors?.email}
-						</FormControl.ErrorMessage>
-					</FormControl>
-					<FormControl isInvalid={errors?.password}>
-						<FormControl.Label>Password</FormControl.Label>
-						<Input
-							type="password"
-							onChangeText={text =>
-								handleChange("password", text)
-							}
-						/>
+							<FormControl.ErrorMessage>
+								{errors?.email}
+							</FormControl.ErrorMessage>
+						</FormControl>
+						<FormControl isInvalid={errors?.password}>
+							<FormControl.Label>Password</FormControl.Label>
+							<Input
+								type="password"
+								onChangeText={text =>
+									handleChange("password", text)
+								}
+							/>
 
-						<FormControl.ErrorMessage>
-							{errors?.password}
-						</FormControl.ErrorMessage>
-					</FormControl>
-					<FormControl isInvalid={errors?.confirmPassword}>
-						<FormControl.Label>Confirm Password</FormControl.Label>
-						<Input
-							type="password"
-							onChangeText={text =>
-								handleChange("confirmPassword", text)
-							}
-						/>
+							<FormControl.ErrorMessage>
+								{errors?.password}
+							</FormControl.ErrorMessage>
+						</FormControl>
+						<FormControl isInvalid={errors?.confirmPassword}>
+							<FormControl.Label>
+								Confirm Password
+							</FormControl.Label>
+							<Input
+								type="password"
+								onChangeText={text =>
+									handleChange("confirmPassword", text)
+								}
+							/>
 
-						<FormControl.ErrorMessage>
-							{errors?.confirmPassword}
-						</FormControl.ErrorMessage>
-					</FormControl>
-					<FormControl isInvalid={errors?.macAddress}>
-						<FormControl.Label>Mac Address</FormControl.Label>
-						<Input
-							onChangeText={text => {
-								const strippedMac = text
-									.replace(/^a-zA-Z0-9]/g, ":")
-									.trim();
-								handleChange("macAddress", strippedMac);
-							}}
-						/>
+							<FormControl.ErrorMessage>
+								{errors?.confirmPassword}
+							</FormControl.ErrorMessage>
+						</FormControl>
+						<FormControl isInvalid={errors?.macAddress}>
+							<FormControl.Label>Mac Address</FormControl.Label>
+							<Input
+								onChangeText={text => {
+									const strippedMac = text
+										.replace(/^a-zA-Z0-9]/g, ":")
+										.trim();
+									handleChange("macAddress", strippedMac);
+								}}
+							/>
 
-						<FormControl.ErrorMessage>
-							{errors?.macAddress}
-						</FormControl.ErrorMessage>
-					</FormControl>
-					<Button
-						mt="2"
-						colorScheme="indigo"
-						onPress={handleSubmit}
-						_disabled={isSubmitting}>
-						Sign up
-					</Button>
-					<HStack mt="6" alignItems="center" justifyContent="center">
-						<Text
-							fontSize="sm"
-							color="coolGray.800"
-							_dark={{
-								color: "warmGray.50",
-							}}
-							fontWeight={400}>
-							Already have an account?{" "}
-						</Text>
+							<FormControl.ErrorMessage>
+								{errors?.macAddress}
+							</FormControl.ErrorMessage>
+						</FormControl>
 						<Button
-							variant="link"
-							onPress={() => {
-								navigation.goBack();
-							}}>
-							<Text
-								fontWeight="bold"
-								color="indigo.500"
-								underline
-								_dark={{
-									color: "indigo.300",
-								}}>
-								Sign In
-							</Text>
+							mt="2"
+							colorScheme="indigo"
+							onPress={handleSubmit}
+							_disabled={isSubmitting}>
+							Sign up
 						</Button>
-					</HStack>
-				</VStack>
-			</Box>
-		</Center>
+						<HStack
+							mt="6"
+							alignItems="center"
+							justifyContent="center">
+							<Text
+								fontSize="sm"
+								color="coolGray.800"
+								_dark={{
+									color: "warmGray.50",
+								}}
+								fontWeight={400}>
+								Already have an account?{" "}
+							</Text>
+							<Button
+								variant="link"
+								onPress={() => {
+									navigation.goBack();
+								}}>
+								<Text
+									fontWeight="bold"
+									color="indigo.500"
+									underline
+									_dark={{
+										color: "indigo.300",
+									}}>
+									Sign In
+								</Text>
+							</Button>
+						</HStack>
+					</VStack>
+				</Box>
+			</Center>
+		</Box>
 	);
 }
