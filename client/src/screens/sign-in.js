a27231fb938de9a5b17e49d1602b2ecbe4a0b8d8
bobikenobi12 <borislav1207@gmail.com>
2023-03-25 16:08:04 +0200

    added theming for unathenticated users

--- client/src/screens/sign-in.js
+++ client/src/screens/sign-in.js
@@ -10,6 +10,7 @@ import {
 	Button,
 	HStack,
 	Text,
+	useColorModeValue,
 } from "native-base";
 import { useFormValidation } from "../hooks/useFormValidation";
 import { validateSignInForm } from "../utils/validation";
@@ -20,95 +21,105 @@ export default function SignInScreen({ navigation }) {
 		useFormValidation({ email: "", password: "" }, validateSignInForm);
 
 	const { login } = useContext(AuthContext);
-	return (
-		<Center w="100%">
-			<Box safeArea p="2" py="8" w="90%" maxW="290">
-				<Heading
-					size="lg"
-					fontWeight="600"
-					color="coolGray.800"
-					_dark={{
-						color: "warmGray.50",
-					}}>
-					Welcome
-				</Heading>
-				<Heading
-					mt="1"
-					_dark={{
-						color: "warmGray.200",
-					}}
-					color="coolGray.600"
-					fontWeight="medium"
-					size="xs">
-					Sign in to continue!
-				</Heading>
+	const bg = useColorModeValue("white", "coolGray.800");
+	const textColor = useColorModeValue("coolGray.800", "warmGray.50");
 
-				<VStack space={3} mt="5">
-					<FormControl>
-						<FormControl.Label>Email</FormControl.Label>
-						<Input
-							onChangeText={text => handleChange("email", text)}
-						/>
-						<FormControl.HelperText>
-							We'll never share your email.
-						</FormControl.HelperText>
-						{errors.email && (
-							<FormControl.ErrorMessage>
-								{errors.email}
-							</FormControl.ErrorMessage>
-						)}
-					</FormControl>
-					<FormControl>
-						<FormControl.Label>Password</FormControl.Label>
-						<Input
-							type="password"
-							onChangeText={text =>
-								handleChange("password", text)
-							}
-						/>
-						{errors.password && (
-							<FormControl.ErrorMessage>
-								{errors.password}
-							</FormControl.ErrorMessage>
-						)}
-					</FormControl>
-					<Button
-						mt="2"
-						colorScheme="indigo"
-						onPress={async () => {
-							console.log(values);
-							await login(values).catch(alert);
+	return (
+		<Box flex="1" safeArea bg={bg} width="100%">
+			<Center w="100%">
+				<Box safeArea p="2" py="8" w="90%" maxW="290">
+					<Heading
+						size="lg"
+						fontWeight="600"
+						color="coolGray.800"
+						_dark={{
+							color: "warmGray.50",
+						}}>
+						Welcome
+					</Heading>
+					<Heading
+						mt="1"
+						_dark={{
+							color: "warmGray.200",
 						}}
-						_disabled={isSubmitting}>
-						Sign in
-					</Button>
-					<HStack mt="6" alignItems="center" justifyContent="center">
-						<Text
-							fontSize="sm"
-							color="coolGray.600"
-							_dark={{
-								color: "warmGray.200",
-							}}>
-							I'm a new user.{" "}
-						</Text>
+						color="coolGray.600"
+						fontWeight="medium"
+						size="xs">
+						Sign in to continue!
+					</Heading>
+
+					<VStack space={3} mt="5">
+						<FormControl>
+							<FormControl.Label>Email</FormControl.Label>
+							<Input
+								onChangeText={text =>
+									handleChange("email", text)
+								}
+							/>
+							<FormControl.HelperText>
+								We'll never share your email.
+							</FormControl.HelperText>
+							{errors.email && (
+								<FormControl.ErrorMessage>
+									{errors.email}
+								</FormControl.ErrorMessage>
+							)}
+						</FormControl>
+						<FormControl>
+							<FormControl.Label>Password</FormControl.Label>
+							<Input
+								type="password"
+								onChangeText={text =>
+									handleChange("password", text)
+								}
+							/>
+							{errors.password && (
+								<FormControl.ErrorMessage>
+									{errors.password}
+								</FormControl.ErrorMessage>
+							)}
+						</FormControl>
 						<Button
-							variant="link"
-							onPress={() => {
-								navigation.navigate("Sign Up");
-							}}>
-							<Text
-								fontWeight="bold"
-								color="indigo.500"
-								underline
-								_dark={{
-									color: "indigo.300",
-								}}>
-								Sign Up
-							</Text>
+							mt="2"
+							colorScheme="indigo"
+							onPress={async () => {
+								console.log(values);
+								await login(values).catch(alert);
+							}}
+							_disabled={isSubmitting}>
+							Sign in
 						</Button>
-					</HStack>
-				</VStack>
-			</Box>
-		</Center>
+						<HStack
+							mt="6"
+							alignItems="center"
+							justifyContent="center">
+							<Text
+								fontSize="sm"
+								color="coolGray.600"
+								_dark={{
+									color: "warmGray.200",
+								}}>
+								I'm a new user.{" "}
+							</Text>
+							<Button
+								variant="link"
+								onPress={() => {
+									navigation.navigate("Sign Up");
+								}}>
+								<Text
+									fontWeight="bold"
+									color="indigo.500"
+									underline
+									_dark={{
+										color: "indigo.300",
+									}}>
+									Sign Up
+								</Text>
+							</Button>
+						</HStack>
+					</VStack>
+				</Box>
+			</Center>
+		</Box>
 	);
 }
--- client/src/screens/sign-up.js
+++ client/src/screens/sign-up.js
@@ -9,6 +9,7 @@ import {
 	Button,
 	HStack,
 	Text,
+	useColorModeValue,
 } from "native-base";
 import { useFormValidation } from "../hooks/useFormValidation";
 import { validateSignUpForm } from "../utils/validation";
@@ -26,135 +27,146 @@ export default function SignUpScreen({ navigation }) {
 			},
 			validateSignUpForm
 		);
+	const bg = useColorModeValue("white", "coolGray.800");
+	const textColor = useColorModeValue("coolGray.800", "warmGray.50");
 	const { register } = useContext(AuthContext);
 	return (
-		<Center w="100%">
-			<Box safeArea p="2" w="90%" maxW="290" py="8">
-				<Heading
-					size="lg"
-					color="coolGray.800"
-					_dark={{
-						color: "warmGray.50",
-					}}
-					fontWeight="semibold">
-					Welcome
-				</Heading>
-				<Heading
-					mt="1"
-					color="coolGray.600"
-					_dark={{
-						color: "warmGray.200",
-					}}
-					fontWeight="medium"
-					size="xs">
-					Sign up to continue!
-				</Heading>
-				<VStack space={3} mt="5">
-					<FormControl>
-						<FormControl.Label>Username</FormControl.Label>
-						<Input
-							onChangeText={text =>
-								handleChange("username", text)
-							}
-						/>
-						{errors?.username && (
-							<FormControl.ErrorMessage>
-								{errors?.username}
-							</FormControl.ErrorMessage>
-						)}
-					</FormControl>
-					<FormControl>
-						<FormControl.Label>Email</FormControl.Label>
-						<Input
-							onChangeText={text => handleChange("email", text)}
-						/>
-						{errors?.email && (
-							<FormControl.ErrorMessage>
-								{errors?.email}
-							</FormControl.ErrorMessage>
-						)}
-					</FormControl>
-					<FormControl>
-						<FormControl.Label>Password</FormControl.Label>
-						<Input
-							type="password"
-							onChangeText={text =>
-								handleChange("password", text)
-							}
-						/>
-						{errors?.password && (
-							<FormControl.ErrorMessage>
-								{errors?.password}
-							</FormControl.ErrorMessage>
-						)}
-					</FormControl>
-					<FormControl>
-						<FormControl.Label>Confirm Password</FormControl.Label>
-						<Input
-							type="password"
-							onChangeText={text =>
-								handleChange("confirmPassword", text)
-							}
-						/>
-						{errors?.confirmPassword && (
-							<FormControl.ErrorMessage>
-								{errors?.confirmPassword}
-							</FormControl.ErrorMessage>
-						)}
-					</FormControl>
-					<FormControl>
-						<FormControl.Label>Mac Address</FormControl.Label>
-						<Input
-							onChangeText={text => {
-								const strippedMac = text
-									.replace(/^a-zA-Z0-9]/g, ":")
-									.trim();
-								handleChange("macAddress", strippedMac);
-							}}
-						/>
-						{errors?.macAdress && (
-							<FormControl.ErrorMessage>
-								{errors?.macAdress}
-							</FormControl.ErrorMessage>
-						)}
-					</FormControl>
-					<Button
-						mt="2"
-						colorScheme="indigo"
-						onPress={async () => {
-							await register(values).catch(alert);
+		<Box flex="1" safeArea bg={bg} width="100%">
+			<Center w="100%">
+				<Box safeArea p="2" w="90%" maxW="290" py="8">
+					<Heading
+						size="lg"
+						color="coolGray.800"
+						_dark={{
+							color: "warmGray.50",
 						}}
-						isDisabled={isSubmitting}>
-						Sign up
-					</Button>
-					<HStack mt="6" alignItems="center" justifyContent="center">
-						<Text
-							fontSize="sm"
-							color="coolGray.800"
-							_dark={{
-								color: "warmGray.50",
-							}}
-							fontWeight={400}>
-							Already have an account?{" "}
-						</Text>
+						fontWeight="semibold">
+						Welcome
+					</Heading>
+					<Heading
+						mt="1"
+						color="coolGray.600"
+						_dark={{
+							color: "warmGray.200",
+						}}
+						fontWeight="medium"
+						size="xs">
+						Sign up to continue!
+					</Heading>
+					<VStack space={3} mt="5">
+						<FormControl>
+							<FormControl.Label>Username</FormControl.Label>
+							<Input
+								onChangeText={text =>
+									handleChange("username", text)
+								}
+							/>
+							{errors?.username && (
+								<FormControl.ErrorMessage>
+									{errors?.username}
+								</FormControl.ErrorMessage>
+							)}
+						</FormControl>
+						<FormControl>
+							<FormControl.Label>Email</FormControl.Label>
+							<Input
+								onChangeText={text =>
+									handleChange("email", text)
+								}
+							/>
+							{errors?.email && (
+								<FormControl.ErrorMessage>
+									{errors?.email}
+								</FormControl.ErrorMessage>
+							)}
+						</FormControl>
+						<FormControl>
+							<FormControl.Label>Password</FormControl.Label>
+							<Input
+								type="password"
+								onChangeText={text =>
+									handleChange("password", text)
+								}
+							/>
+							{errors?.password && (
+								<FormControl.ErrorMessage>
+									{errors?.password}
+								</FormControl.ErrorMessage>
+							)}
+						</FormControl>
+						<FormControl>
+							<FormControl.Label>
+								Confirm Password
+							</FormControl.Label>
+							<Input
+								type="password"
+								onChangeText={text =>
+									handleChange("confirmPassword", text)
+								}
+							/>
+							{errors?.confirmPassword && (
+								<FormControl.ErrorMessage>
+									{errors?.confirmPassword}
+								</FormControl.ErrorMessage>
+							)}
+						</FormControl>
+						<FormControl>
+							<FormControl.Label>Mac Address</FormControl.Label>
+							<Input
+								onChangeText={text => {
+									const strippedMac = text
+										.replace(/^a-zA-Z0-9]/g, ":")
+										.trim();
+									handleChange("macAddress", strippedMac);
+								}}
+							/>
+							{errors?.macAdress && (
+								<FormControl.ErrorMessage>
+									{errors?.macAdress}
+								</FormControl.ErrorMessage>
+							)}
+						</FormControl>
 						<Button
-							variant="link"
-							onPress={() => {
-								navigation.goBack();
-							}}>
-							<Text
-								fontWeight="bold"
-								color="indigo.500"
-								underline
-								_dark={{
-									color: "indigo.300",
-								}}>
-								Sign In
-							</Text>
+							mt="2"
+							colorScheme="indigo"
+							onPress={async () => {
+								await register(values).catch(alert);
+							}}
+							isDisabled={isSubmitting}>
+							Sign up
 						</Button>
-					</HStack>
-				</VStack>
-			</Box>
-		</Center>
+						<HStack
+							mt="6"
+							alignItems="center"
+							justifyContent="center">
+							<Text
+								fontSize="sm"
+								color="coolGray.800"
+								_dark={{
+									color: "warmGray.50",
+								}}
+								fontWeight={400}>
+								Already have an account?{" "}
+							</Text>
+							<Button
+								variant="link"
+								onPress={() => {
+									navigation.goBack();
+								}}>
+								<Text
+									fontWeight="bold"
+									color="indigo.500"
+									underline
+									_dark={{
+										color: "indigo.300",
+									}}>
+									Sign In
+								</Text>
+							</Button>
+						</HStack>
+					</VStack>
+				</Box>
+			</Center>
+		</Box>
 	);
 }
