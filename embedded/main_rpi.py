532fdc6dc4063f9ce41c56d3c8f74e55c37cf2d3
yasenOfficial <91755243+yasenOfficial@users.noreply.github.com>
2023-04-09 12:25:39 +0300

    Update main_rpi.py

--- embedded/main_rpi.py
+++ embedded/main_rpi.py
@@ -6,11 +6,40 @@ import requests
 
 from dotenv import load_dotenv
 
-import os
 from Crypto.Cipher import AES
 from Crypto.Util import Padding
 
+import read
+import write
+
+import binascii
+
+from pn532pi import Pn532, pn532
+from pn532pi import Pn532I2c
+from pn532pi import Pn532Spi
+from pn532pi import Pn532Hsu
+
+import json
+
 load_dotenv()
+
+# Set the desired interface to True
+SPI = False
+I2C = True
+HSU = False
+
+if SPI:
+    PN532_SPI = Pn532Spi(Pn532Spi.SS0_GPIO8)
+    nfc = Pn532(PN532_SPI)
+# When the number after #elif set as 1, it will be switch to HSU Mode
+elif HSU:
+    PN532_HSU = Pn532Hsu(0)
+    nfc = Pn532(PN532_HSU)
+
+# When the number after #if & #elif set as 0, it will be switch to I2C Mode
+elif I2C:
+    PN532_I2C = Pn532I2c(1)
+    nfc = Pn532(PN532_I2C)
 
 #ENCRYPTION, DECRYPTION CODE
 ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY").encode("utf-8")
@@ -33,25 +62,10 @@ def decrypt(text):
     return unpadded.decode('utf-8')
 
 
-# def expand2square(pil_img, background_color):
-#     width, height = pil_img.size
-#     if width == height:
-#         return pil_img
-#     elif width > height:
-#         result = Image.new(pil_img.mode, (width, width), background_color)
-#         result.paste(pil_img, (0, (width - height) // 2))
-#         return result
-#     else:
-#         result = Image.new(pil_img.mode, (height, height), background_color)
-#         result.paste(pil_img, ((height - width) // 2, 0))
-#         return result
 
-
-# {'status': True, 'nickname': 'Test', 'pending': True}
-# Ako status == True iziskva action osven ako pendinga ne e true
 
 def get_status_checker():
-    url = "https://13ab-95-42-52-106.eu.ngrok.io/statusupdate"
+    url = "https://a37d-95-42-52-106.ngrok-free.app/statusupdate"
     while True:
         headers = {'X-MAC-Address' : '00:00:00:00:00:00'}
         response = requests.get(url, headers = headers)
@@ -65,31 +79,58 @@ def get_status_checker():
         
         if raspiSend["status"]:
             requests.post(f"{url}/pending", headers=headers)
+                        
+            read.setup_read()
+
+            read_data = None
             
-            
-            # nie prastame na servera VIKAM READ ARDUINO!!! nfc
-            
-            create_tag(raspiSend["_owner"], raspiSend["nickname"], "nfc", "test")
+            while True:
+                print("Waiting for an ISO14443A card")
+                try: 
+                    read_data = read.loop_read()
+                    print(f"89: {read_data}")
+                    if not read_data:
+                        continue
+                    break
+                except Exception:
+                    pass
+
+            create_tag(raspiSend["_owner"], raspiSend["nickname"], "nfc", read_data)
             
             requests.get(f"{url}/resolve", headers=headers)
+
+
             
         elif raspiReceive["status"]:
+            is_written = False
+            print("In raspiReceive")
             requests.post(f"{url}/pending", headers=headers)
             
             data = decrypt(raspiReceive["tag"]["data"])
             print(data)
             
-            
-            # NA ARDUINOTO WRITE NA TAGA NFC
+            write.setup_write()
+
+            while True:
+                try: 
+                    is_written = write.loop_write(data)
+                    if not is_written:
+                        continue
+                    break
+
+                except Exception:
+                    pass
             
             requests.get(f"{url}/resolve", headers=headers)
             
         
 def create_tag(_owner, nickname, type, data):
     headers = {"X-Mac-Address": "00:00:00:00:00:00"}
-    data = {"_owner": _owner, "nickname": nickname, "type": type, "data": data}
+    # byte_string = bytes.fromhex(data)
+
+    data = {"_owner": _owner, "nickname": nickname, "type": type, "data": data.decode()}
     
-    url = "https://13ab-95-42-52-106.eu.ngrok.io/jrn/tags/"
+    url = "https://a37d-95-42-52-106.ngrok-free.app/jrn/tags/"
     
     res = requests.post(url, headers=headers, json=data)
     print(res.json())
@@ -98,12 +139,8 @@ def create_tag(_owner, nickname, type, data):
        
 
 if __name__ == '__main__':
-    get_status_checker()
-    
-    
-    
-    
-
-    
-
-
+    while True:
+        try:
+            get_status_checker()
+        except Exception:
+            pass
